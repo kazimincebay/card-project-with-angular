237c05ffdebcd2bdc636f5c17e73b35048cf5b7c
Kazım İncebay <kazimincebay@gmail.com>
2025-08-02 08:41:41 +0300

    I finished this project
    - I created array in app.component.ts
    - I sent datas app.component.ts to card.component.ts
    - I created three cards and added datas this cards
    - I used ngFor for cards

--- kart/src/app/app.component.ts
+++ kart/src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'kart';
+  posts = [
+    {
+      title: 'Dağ Bisikleti',
+      imageUrl: 'assets/biking.jpeg',
+      username: '@kzmdag',
+      content: 'Çok hızlı sürdüm',
+    },
+    {
+      title: 'Tırmanış',
+      imageUrl: 'assets/mountain.jpeg',
+      username: '@kzmuludag',
+      content: 'Bugünde iyi tırmandım',
+    },
+    {
+      title: 'Doğa Yürüyüşü',
+      imageUrl: 'assets/tree.jpeg',
+      username: '@kzmdoga',
+      content: "Hadi biraz Maçka Park'ında turlayalım",
+    }
+  ];
 }
--- kart/src/app/card/card.component.ts
+++ kart/src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-
+@Input() title:string='';
+@Input() imageUrl:string='';
+@Input() username:string='';
+@Input() content:string='';
   constructor() { }
 
   ngOnInit(): void {
